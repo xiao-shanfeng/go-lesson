fd8793f0267000fecd4596d1b690b5c5bed1b841
liupf <pan_fengliu@163.com>
2022-04-17 00:56:32 +0800

    go map基本使用

--- main.go
+++ main.go
@@ -3,37 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	// go 数组和切片
-	arr1 := [...]int{0, 1, 2, 3, 4, 5, 6}
-	//c1 := arr1[2:]
-	//c1 := arr1[:]
-	c1 := arr1[2:5]
-	//c1 := arr1[:5]
-	//c1[0] = 7
-	fmt.Println(c1)
-	fmt.Println(arr1)
-	fmt.Println(len(c1), cap(c1))
+	// map的使用
+	m1 := make(map[string]string)
+	m1["name"] = "Tom"
+	m1["age"] = "12"
+	fmt.Println(m1)
+	fmt.Printf("%T\n", m1)
 
-	// 使用make创建切片
-	//c2 := make([]int, 4, 6)
-	c2 := make([]int, 4)
-	//var c2 []int
-	c2[0] = 0
-	c2[1] = 1
-	c2[2] = 2
-	c2[3] = 3
-	c2 = append(c2, 4)
-	c2 = append(c2, 5)
-	c2 = append(c2, 6)
-	c2 = append(c2, 7)
-	c2 = append(c2, 8)
-	fmt.Println(c2, len(c2), cap(c2))
+	// 下面这种声明必须要初始化一下
+	var m2 map[int]int
+	m2 = map[int]int{}
+	m2[1] = 1
+	m2[2] = 2
+	fmt.Println(m2)
 
-	// copy复制切片
-	// arr1 [0,1,2,3,4,5,6]
-	c3 := arr1[:]
-	c4 := arr1[2:3]
-	// copy 把后者覆盖前者
-	copy(c3, c4)
-	fmt.Println(c3, c4) //2,1,2,3,4,5,6   2
+	m3 := map[int]string{1: "tom", 2: "jack"}
+	fmt.Println(m3)
+
+	m4 := make(map[int]interface{})
+	m4[1] = "tom"
+	m4[2] = 12
+	m4[3] = true
+	m4[4] = [...]int{1, 2, 3, 4}
+	fmt.Println(m4, len(m4))
+
+	delete(m4, 4)
+	fmt.Println(m4, len(m4))
+
+	for k, v := range m4 {
+		fmt.Println(k, v)
+	}
 }
