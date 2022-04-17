73fcf4afb6bcbe715933513978cd8a85976a01c2
liupf <pan_fengliu@163.com>
2022-04-17 14:01:32 +0800

    go切片基本使用

--- main.go
+++ main.go
@@ -4,27 +4,36 @@ import "fmt"
 
 func main() {
 	// go 数组和切片
-	// 定义数组 var 变量名 = [变量长度]变量类型{变量值1,变量值2,...,变量值n}
-	var arr1 = [4]int{1, 2, 3, 4}
-	fmt.Printf("%T\n", arr1)
+	arr1 := [...]int{0, 1, 2, 3, 4, 5, 6}
+	//c1 := arr1[2:]
+	//c1 := arr1[:]
+	c1 := arr1[2:5]
+	//c1 := arr1[:5]
+	//c1[0] = 7
+	fmt.Println(c1)
 	fmt.Println(arr1)
-	var arr2 = [...]int{1, 2, 3, 4, 5}
-	fmt.Printf("%T\n", arr2)
-	fmt.Println(arr2)
-	var arr3 [5]int
-	fmt.Println(arr3)
-	arr4 := new([5]int)
-	arr4[3] = 3
-	fmt.Println(arr4)
+	fmt.Println(len(c1), cap(c1))
 
-	for i := 0; i < len(arr1); i++ {
-		fmt.Println(arr1[i])
-	}
-	for i, v := range arr1 {
-		fmt.Println(i, v)
-	}
+	// 使用make创建切片
+	//c2 := make([]int, 4, 6)
+	c2 := make([]int, 4)
+	//var c2 []int
+	c2[0] = 0
+	c2[1] = 1
+	c2[2] = 2
+	c2[3] = 3
+	c2 = append(c2, 4)
+	c2 = append(c2, 5)
+	c2 = append(c2, 6)
+	c2 = append(c2, 7)
+	c2 = append(c2, 8)
+	fmt.Println(c2, len(c2), cap(c2))
 
-	// 数组长度len，数组容量cap
-	fmt.Println(len(arr1), cap(arr1))
-
+	// copy复制切片
+	// arr1 [0,1,2,3,4,5,6]
+	c3 := arr1[:]
+	c4 := arr1[2:3]
+	// copy 把后者覆盖前者
+	copy(c3, c4)
+	fmt.Println(c3, c4) //2,1,2,3,4,5,6   2
 }
