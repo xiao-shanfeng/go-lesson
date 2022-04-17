39f0947619978f9942b6bc375603b992ab7bb580
liupf <pan_fengliu@163.com>
2022-04-17 13:46:56 +0800

    go数组基本使用

--- main.go
+++ main.go
@@ -3,75 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	// 自增自减
-	// go 中只有a++，没有++a
-	a := 0
-	a++
-	//a--
-	//++a
-	fmt.Println(a)
+	// go 数组和切片
+	// 定义数组 var 变量名 = [变量长度]变量类型{变量值1,变量值2,...,变量值n}
+	var arr1 = [4]int{1, 2, 3, 4}
+	fmt.Printf("%T\n", arr1)
+	fmt.Println(arr1)
+	var arr2 = [...]int{1, 2, 3, 4, 5}
+	fmt.Printf("%T\n", arr2)
+	fmt.Println(arr2)
+	var arr3 [5]int
+	fmt.Println(arr3)
+	arr4 := new([5]int)
+	arr4[3] = 3
+	fmt.Println(arr4)
 
-	// 条件语句
-	if a > 10 {
-		fmt.Println("这是正确的")
-	} else if a == -1 {
-		fmt.Println("中间情况")
-	} else {
-		fmt.Println("这是错误的")
+	for i := 0; i < len(arr1); i++ {
+		fmt.Println(arr1[i])
+	}
+	for i, v := range arr1 {
+		fmt.Println(i, v)
 	}
 
-	// 选择语句
-	// go中switch语句中，case自带的break，如果想实现别的语言的穿透，使用fallthrough
-	switch a {
-	case 0:
-		fmt.Println("0")
-	case 1:
-		fmt.Println(1)
-		fallthrough
-	case 2:
-		fmt.Println(2)
-		fallthrough
-	default:
-		fmt.Println("这是默认情况")
-	}
+	// 数组长度len，数组容量cap
+	fmt.Println(len(arr1), cap(arr1))
 
-	// 循环语句
-	for {
-		a++
-		if a == 5 {
-			break
-		}
-		fmt.Println(a)
-	}
-
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
-	}
-
-	for a < 10 {
-		fmt.Println(a)
-		a++
-	}
-
-	// 跳转语句
-
-	for i := 0; i < 10; i++ {
-		if i == 5 {
-			continue
-		}
-		fmt.Println(i)
-	}
-
-	//A:
-	for {
-		if a > 15 {
-			//break A
-			goto B
-		}
-		fmt.Println("这是A")
-		a++
-	}
-	//return
-B:
-	fmt.Println("这是B")
 }
